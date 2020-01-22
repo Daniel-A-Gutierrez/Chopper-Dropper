c565b31795a1bc223398d4c3c1d1c30b5b5458f3
Victor Zhou <44218140+Fireryrex@users.noreply.github.com>
2020-01-22 14:46:51 -0800

    Chopper basically done

--- Assets/chopperScript.cs
+++ Assets/chopperScript.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class chopperScript : MonoBehaviour
 {
+
+    [SerializeField] string rightKey;
+    [SerializeField] string leftKey;
+    [SerializeField] float moveSpeed;
+    [SerializeField] string dropKey;
+    [SerializeField] GameObject rock;
+    //[SerializeField] ammoManager gameManager;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,29 @@ public class chopperScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //movement stuff
+        if(Input.GetKey(rightKey))
+        {
+            this.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector3(1 * moveSpeed, 0, 0);
+        }
+        else if(Input.GetKey(leftKey))
+        {
+            this.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector3(-1 * moveSpeed, 0, 0);
+        }
+
+        //rock drop stuff
+        if(Input.GetKeyDown(dropKey))
+        {
+            if(true)//gameManager.checkFlyerAmmo())
+            {
+                GameObject droppedRock = Instantiate(rock, this.gameObject.transform.position, Quaternion.identity);
+                droppedRock.GetComponent<Rigidbody2D>().velocity = this.gameObject.GetComponent<Rigidbody2D>().velocity;
+            }
+            else
+            {
+                //do something
+                Debug.Log("Out of ammo");
+            }
+        }
     }
 }
