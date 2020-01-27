0f82defeff1e0132c284ae37a430e555edfd5272
SupremeEmperor <44217972+SupremeEmperor@users.noreply.github.com>
2020-01-27 14:16:17 -0800

    Lvl 1 change

---
+++ Assets/DeathBox.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathBox : MonoBehaviour
+{
+    [SerializeField] private string TagToKill = "Player";
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == TagToKill)
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
