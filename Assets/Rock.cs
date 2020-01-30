ff59ade36b0a3a55426e358e7b768f9799c6109d
SupremeEmperor <44217972+SupremeEmperor@users.noreply.github.com>
2020-01-29 17:22:37 -0800

    Made some more level 1 stuff

---
+++ Assets/Rock.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : MonoBehaviour
+{
+    [SerializeField] private string playerTag = "Player";
+    [SerializeField] private string enemyTag = "Enemy";
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == enemyTag)
+        {
+            Destroy(collision.gameObject);
+            Destroy(this.gameObject); 
+        }
+        if(collision.gameObject.tag == playerTag)
+        {
+            //Stun player
+            Destroy(this.gameObject);
+        }
+    }
+}
